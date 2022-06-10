c1fe225029b60fefb1cdde72dee016ceddc400e7
MZiob <michalziober08@gmail.com>
2022-06-10 20:46:29 +0200

    added generating, but doesnt work- canPass must be fixed

--- generator/src/main/java/pl/edu/pw/ee/Generator.java
+++ generator/src/main/java/pl/edu/pw/ee/Generator.java
@@ -1,13 +1,20 @@
 package pl.edu.pw.ee;
 
-import pl.edu.pw.ee.exercises.*;
+import java.util.ArrayList;
+import java.util.Random;
 
+import pl.edu.pw.ee.exceptions.CannotPassException;
+import pl.edu.pw.ee.exceptions.IllegalElementsForThisExerciseException;
+import pl.edu.pw.ee.exercises.*;
+import pl.edu.pw.ee.services.Element;
 import pl.edu.pw.ee.services.Exercise;
 
 public class Generator {
     private Exercise[] exercises = new Exercise[21];
+    Node[] nodes;
 
     public Generator(){
+        nodes = Node.createNodes();
         exercises[0] = new Ex1();
         exercises[1] = new Ex2();
         exercises[2] = new Ex3();
@@ -31,7 +38,74 @@ public class Generator {
         exercises[20] = new Ex21();
     }
 
-    // public Exercise generateExercise(){
+    //Przechwytywanie wyjątku wyżej
+    public Exercise generateExercise() throws CannotPassException{
+        Random rand = new Random();
+        ArrayList<Element> elements = new ArrayList<>();
+        ArrayList<Exercise> realPossibleExercises;
+        ArrayList<Node> realPossibleNodes;
+        Node actual = getFirstNode(nodes, rand);
+        try {
+            elements.add(actual.getElement());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        while(true){
+            int[] possibleExercises = actual.getIDsOfPossibleExercises();
+            int[] possibleNextNodes = actual.getNextNodesIDs();
+            realPossibleExercises = getRealPossibleExercises(elements, possibleExercises);
+            realPossibleNodes = getRealPossibleNodes(elements, possibleNextNodes);
+            int id = rand.nextInt(realPossibleExercises.size() + realPossibleNodes.size());
+            if(id < realPossibleExercises.size()){
+                return realPossibleExercises.get(id);
+            }
+            actual = realPossibleNodes.get(id - realPossibleExercises.size());
+            try {
+                elements.add(actual.getElement());
+            } catch (Exception e) {
+                ;
+            }
+        }
+    }
 
-    // }
+    private Node getFirstNode(Node[] nodes, Random r){
+        ArrayList<Node> possiblyNextNodes = new ArrayList<>();
+        for(int i = 0; i < nodes.length; i++){
+            try {
+                if(nodes[i].canPass(new ArrayList<Element>())){
+                    possiblyNextNodes.add(nodes[i]);
+                }
+            } catch (CannotPassException e) {
+                ;
+            }
+        }
+        return nodes[r.nextInt(possiblyNextNodes.size())];
+    }
+
+    private ArrayList<Node> getRealPossibleNodes(ArrayList<Element> elements, int[] possibleNextNodes){
+        ArrayList<Node> realPossibleNodes = new ArrayList<>();
+        for(int i = 0; i < possibleNextNodes.length; i++){
+            try {
+                if(nodes[possibleNextNodes[i]].canPass(elements)){
+                    realPossibleNodes.add(nodes[possibleNextNodes[i]]);
+                }
+            } catch (CannotPassException e) {
+                ;
+            }
+        }
+        return realPossibleNodes;
+    }
+
+    private ArrayList<Exercise> getRealPossibleExercises(ArrayList<Element> elements, int[] possibleExercises){
+        ArrayList<Exercise> realPossibleExercises = new ArrayList<>();
+        for(int i = 0; i < possibleExercises.length; i++){
+            try{
+                exercises[possibleExercises[i]].insertElements(elements);
+                realPossibleExercises.add(exercises[possibleExercises[i]]);
+            }catch(IllegalElementsForThisExerciseException e){
+                ;
+            }
+        }
+        return realPossibleExercises;
+    }
 }
--- generator/src/main/java/pl/edu/pw/ee/Node.java
+++ generator/src/main/java/pl/edu/pw/ee/Node.java
@@ -1,5 +1,7 @@
 package pl.edu.pw.ee;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 
 import pl.edu.pw.ee.exceptions.CannotPassException;
@@ -8,15 +10,14 @@ import pl.edu.pw.ee.services.Element;
 public class Node {
     private int idOfNode;
     private int[] nextNodesIDs;
+    private int[] possibleExercises;
     private Class<?>[][] needed;
     private Class<?> whatNodeGives; //Jaką daną daje Node do puli
 
-    public Node(Class<?>[][] nnn){
-
-    }
-    public Node(int id, int[] nextNodesIDs , Class<?>[][] needed, Class<?> whatGives){
+    public Node(int id, int[] nextNodesIDs, int[] possibleExercises, Class<?>[][] needed, Class<?> whatGives){
         idOfNode = id;
         this.nextNodesIDs = nextNodesIDs;
+        this.possibleExercises = possibleExercises;
         this.needed = needed;
         this.whatNodeGives = whatGives;
     }
@@ -45,21 +46,43 @@ public class Node {
         return true;
     }
 
+    public Element getElement() throws Exception{
+        Constructor cons = whatNodeGives.getConstructor();
+        return  (Element)cons.newInstance();
+    }
+
+    //Giving exercises that can be made from this node
+    public int[] getIDsOfPossibleExercises(){
+        int[] indexesOfPossibleExercises = new int[possibleExercises.length];
+        for(int i = 0; i < possibleExercises.length; i++){
+            indexesOfPossibleExercises[i] = possibleExercises[i] - 1;
+        }
+        return indexesOfPossibleExercises;
+    }
+
+    public int[] getNextNodesIDs(){
+        return nextNodesIDs;
+    }
+    
+    public int getID(){
+        return idOfNode;
+    }
+
     public static Node[] createNodes(){
         Node[] nodes = new Node[13];
-        nodes[0] = new Node(0, new int[]{5, 7, 8}, new Class<?>[][]{}, Point.class);
-        nodes[1] = new Node(1, new int[]{6, 7}, new Class<?>[][]{}, Line.class);
-        nodes[2] = new Node(2, new int[]{5, 6, 8, 9}, new Class<?>[][]{}, Triangle.class);
-        nodes[3] = new Node(3, new int[]{5, 7, 9}, new Class<?>[][]{}, Square.class);
-        nodes[4] = new Node(4, new int[]{7, 8, 9}, new Class<?>[][]{}, Circle.class);
-        nodes[5] = new Node(5, new int[]{}, new Class<?>[][]{{Point.class},{Triangle.class},{Square.class}}, Point.class);
-        nodes[6] = new Node(6, new int[]{}, new Class<?>[][]{{Line.class},{Triangle.class}}, Line.class);
-        nodes[7] = new Node(7, new int[]{11, 12}, new Class<?>[][]{{Point.class},{Line.class},{Square.class},{Circle.class}}, Triangle.class);
-        nodes[8] = new Node(8, new int[]{10, 12}, new Class<?>[][]{{Point.class},{Triangle.class},{Circle.class}}, Square.class);
-        nodes[9] = new Node(9, new int[]{10, 11}, new Class<?>[][]{{Triangle.class},{Square.class},{Circle.class}}, Circle.class);
-        nodes[10] = new Node(10, new int[]{}, new Class<?>[][]{{Square.class, Circle.class}}, Triangle.class);
-        nodes[11] = new Node(11, new int[]{}, new Class<?>[][]{{Triangle.class, Circle.class}}, Square.class);
-        nodes[12] = new Node(12, new int[]{}, new Class<?>[][]{{Triangle.class, Square.class}}, Circle.class);
+        nodes[0] = new Node(0, new int[]{5, 7, 8}, new int[]{}, new Class<?>[][]{}, Point.class);
+        nodes[1] = new Node(1, new int[]{6, 7}, new int[]{10, 14},new Class<?>[][]{}, Line.class);
+        nodes[2] = new Node(2, new int[]{5, 6, 8, 9}, new int[]{5, 6, 11, 12},new Class<?>[][]{}, Triangle.class);
+        nodes[3] = new Node(3, new int[]{5, 7, 9}, new int[]{7, 8, 9, 13},new Class<?>[][]{}, Square.class);
+        nodes[4] = new Node(4, new int[]{7, 8, 9}, new int[]{},new Class<?>[][]{}, Circle.class);
+        nodes[5] = new Node(5, new int[]{}, new int[]{1, 16, 17},new Class<?>[][]{{Point.class},{Triangle.class},{Square.class}}, Point.class);
+        nodes[6] = new Node(6, new int[]{}, new int[]{2, 3, 4, 20},new Class<?>[][]{{Line.class},{Triangle.class}}, Line.class);
+        nodes[7] = new Node(7, new int[]{11, 12}, new int[]{20, 21},new Class<?>[][]{{Point.class},{Line.class},{Square.class},{Circle.class}}, Triangle.class);
+        nodes[8] = new Node(8, new int[]{10, 12}, new int[]{16, 21},new Class<?>[][]{{Point.class},{Triangle.class},{Circle.class}}, Square.class);
+        nodes[9] = new Node(9, new int[]{10, 11}, new int[]{15},new Class<?>[][]{{Triangle.class},{Square.class},{Circle.class}}, Circle.class);
+        nodes[10] = new Node(10, new int[]{}, new int[]{18, 19},new Class<?>[][]{{Square.class, Circle.class}}, Triangle.class);
+        nodes[11] = new Node(11, new int[]{}, new int[]{18, 19},new Class<?>[][]{{Triangle.class, Circle.class}}, Square.class);
+        nodes[12] = new Node(12, new int[]{}, new int[]{18, 19},new Class<?>[][]{{Triangle.class, Square.class}}, Circle.class);
         
         return nodes;
     }
